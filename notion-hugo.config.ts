79d7f5f6720d6c5b078a24fe8d0ca758a11ceb7d
erickau2 <64601280+erickau2@users.noreply.github.com>
2025-05-25 17:40:05 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-Hugo-1fe78d6d8b2f80d8872ec52b7e5ebd4a?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
